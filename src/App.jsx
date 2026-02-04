0031155b68e3c78641f1402b80d96b20ec4f9fef
MeetPatel <pmaxx678@gmail.com>
2026-02-04 14:27:52 +0530

    simple filtering task done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FilterCategory from './task/FilterCategory';
+
+function App() {
+  return (
+    <div className="App">
+      <FilterCategory/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/task/FilterCategory.jsx
@@ -0,0 +1,106 @@
+import React from 'react'
+import { useState } from 'react'
+const FilterCategory = () => {
+    const [selectedCategory, setCategory] = useState('all')
+    let data = [
+        {
+            name: "Rose",
+            category: "flower",
+        },
+        {
+            name: "Lotus",
+            category: "flower"
+        },
+        {
+            name: "Jasmine",
+            category: "flower"
+        },
+        {
+            name: "Sunflower",
+            category: "flower"
+        },
+        {
+            name: "lily",
+            category: "flower"
+        },
+        {
+            name: "Lion",
+            category: "animal"
+        },
+        {
+            name: "Tiger",
+            category: "animal"
+        },
+        {
+            name: "Fox",
+            category: "animal"
+        },
+        {
+            name: "Wolf",
+            category: "animal"
+        },
+        {
+            name: "Dog",
+            category: "animal"
+        },
+        {
+            name: "Shirt",
+            category: "Clothes"
+        },
+        {
+            name: "T-shirt",
+            category: "Clothes"
+        },
+        {
+            name: "Jacket",
+            category: "Clothes"
+        },
+        {
+            name: "Cargo",
+            category: "Clothes"
+        },
+        {
+            name: "Jeans",
+            category: "Clothes"
+        },
+
+    ]
+    function changeCategory(e) {
+        setCategory(e.target.value)
+    }
+    const filterdata = data.filter((value) => {
+        if (selectedCategory === 'all') {
+            return true
+        }
+        else {
+            return value.category === selectedCategory
+        }
+    })
+    return (
+        <div>
+            <input type="button" value="all" onClick={changeCategory} />
+            <input type="button" value="flower" onClick={changeCategory} />
+            <input type="button" value="animal" onClick={changeCategory} />
+            <input type="button" value="Clothes" onClick={changeCategory} />
+            <table border={1} style={{ margin: "auto", marginTop: "30px" }}>
+                <tr>
+                    <th>Name</th>
+                    <th>Category</th>
+                </tr>
+                {
+                    filterdata.map((item, index) => {
+                        return (
+                            <tr>
+                                <td>{item.name}</td>
+                                <td>{item.category}</td>
+
+                            </tr>
+                        )
+                    })
+                }
+            </table>
+        </div>
+    )
+}
+
+export default FilterCategory
